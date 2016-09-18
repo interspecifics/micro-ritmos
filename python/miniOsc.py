4a7fd0da005173c804329087b9c017e1717a336b
Microhom <microhom@gmail.com>
2016-09-18 08:52:39 -0500

    removed

--- python/miniOsc.py
+++
@@ -1,7 +0,0 @@
-import OSC
-c = OSC.OSCClient()
-c.connect(('127.0.0.1', 57120))   # connect to SuperCollider
-oscmsg = OSC.OSCMessage()
-oscmsg.setAddress("/startup")
-oscmsg.append('HELLO')
-c.send(oscmsg)

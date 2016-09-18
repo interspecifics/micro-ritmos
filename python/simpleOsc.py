4ee07dd10a075dcf2cff22e56a473afdfdb55b90
Microhom <microhom@gmail.com>
2016-09-18 09:31:22 -0500

    added random value

--- python/simpleOsc.py
+++ python/simpleOsc.py
@@ -1,17 +1,21 @@
 from OSC import OSCClient, OSCMessage
+import random
+
 
 OSC_OUT_HOST = "localhost"
-OSC_OUT_PORT = 57120
+OSC_OUT_PORT = 57121
 
 if __name__ == "__main__":
     mOscClient = OSCClient()
     mOscClient.connect( (OSC_OUT_HOST,OSC_OUT_PORT) )
     mOscMessage = OSCMessage()
 
+
     try:
-                    mOscMessage.setAddress("/direccion/")
-                    mOscMessage.append('nombre')
-                    mOscMessage.append('valor')
+                    mOscMessage.setAddress("/direccion")
+                    mOscMessage.append(random.random())
+                    mOscMessage.append('/valor')
+                    mOscMessage.append('/nombre')
                     mOscClient.send(mOscMessage)
 
     except KeyboardInterrupt:

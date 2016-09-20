0e5b94aa0f22763a9d1d89142c8ec42942d8e447
Microhom <microhom@gmail.com>
2016-09-20 12:37:22 -0500

    send to to other host

--- python/simpleOsc.py
+++ python/simpleOsc.py
@@ -2,7 +2,7 @@ from OSC import OSCClient, OSCMessage
 import random
 
 
-OSC_OUT_HOST = "localhost"
+OSC_OUT_HOST = 10.40.24.105
 OSC_OUT_PORT = 57121
 
 if __name__ == "__main__":
@@ -10,12 +10,11 @@ if __name__ == "__main__":
     mOscClient.connect( (OSC_OUT_HOST,OSC_OUT_PORT) )
     mOscMessage = OSCMessage()
 
-
     try:
                     mOscMessage.setAddress("/direccion")
                     mOscMessage.append(random.random())
-                    mOscMessage.append('/valor')
-                    mOscMessage.append('/nombre')
+                    mOscMessage.append(random.random())
+                    mOscMessage.append(random.random())
                     mOscClient.send(mOscMessage)
 
     except KeyboardInterrupt:

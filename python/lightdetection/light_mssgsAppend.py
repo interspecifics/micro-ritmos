fd8b9ae8b5506f2dab2a872a4c1bba74623d6b39
Microhom <microhom@gmail.com>
2016-09-23 19:08:30 -0500

    fix ip

--- python/lightdetection/light_mssgsAppend.py
+++ python/lightdetection/light_mssgsAppend.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
 	SM = stateMachine(rId, w/10)
 
 	## osc init
-	send_addr = '192.168.5.150', 57120
+	send_addr = '192.168.1.150', 57120
 	#send_addr = "localhost", 8300
 
 	cOsc = OSC.OSCClient()

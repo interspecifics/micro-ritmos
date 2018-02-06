ff47a85f3e6722c6ef565f61122fbc86ca1ed1bb
Microhom <microhom@gmail.com>
2018-02-05 19:28:25 -0500

    update

--- python/detection /light_mssgsM.py
+++ python/detection /light_mssgsM.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
 	SM = stateMachine(rId, w/10)
 
 	## osc init
-	send_addr = '127.0.0.1', 8666
+	send_addr = '192.68.1.44', 8666
 	#send_addr = "localhost", 8300
 
 	cOsc = OSC.OSCClient()
